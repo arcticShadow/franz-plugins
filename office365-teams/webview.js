c043da8f00c9cff0d075c9c626a2fd0ad4a291a9
Cole Diffin <cole.diffin@orionhealth.com>
2017-04-12 16:28:16 +1200

    added message notification to Teams

--- office365-teams/webview.js
+++ office365-teams/webview.js
@@ -1,3 +1,15 @@
 module.exports = (Franz, options) => {
-  // No notifications yet
+  const checkForMessages = () => {
+    let unreadCount = 0;
+    const titleChunks = document.title.split(' ')
+    if (titleChunks[0].indexOf('(') === 0) {
+      unreadCount = titleChunks[0].replace('(','').replace(')','')
+    }
+
+    Franz.setBadge(unreadCount);
+  }
+
+  Franz.loop(checkForMessages);
+
+  
 };
